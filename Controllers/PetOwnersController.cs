9d84a87de4630ea8f7e99d01973c497b8629230c
SJCMN <sam.clavette@gmail.com>
2021-12-02 16:30:28 -0600

    add put route, update db

--- Controllers/PetOwnersController.cs
+++ Controllers/PetOwnersController.cs
@@ -58,6 +58,17 @@ namespace pet_hotel.Controllers
             return NoContent();
         }
 
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, PetOwner petOwner) 
+        {
+            Console.WriteLine("in PUT");
+            if ( id != petOwner.id) {
+                return BadRequest();
+            }
         
+            _context.Update(petOwner);
+            _context.SaveChanges();
+            return NoContent();
+        }
     }
 }
