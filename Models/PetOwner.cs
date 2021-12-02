ee771366673533ee2ae88895e7f2a959bba00cac
SJCMN <sam.clavette@gmail.com>
2021-12-02 13:51:25 -0600

    pet owner model

--- Models/PetOwner.cs
+++ Models/PetOwner.cs
@@ -5,5 +5,32 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 namespace pet_hotel
 {
-    public class PetOwner {}
+    public class PetOwner
+    {
+        public int id { get; set; }
+
+        [Required]
+
+        public string name { get; set; }
+
+        [Required]
+
+        public string emailAddress { get; set; }
+        // validate format
+
+        [JsonIgnore]
+
+        public List<Pet> ownersPets { get; set; }
+
+        [NotMapped]
+
+        public int petCount
+        {
+            get
+            {
+                return (this.ownersPets != null ? this.ownersPets.Count : 0);
+            }
+        }
+
+    }
 }
