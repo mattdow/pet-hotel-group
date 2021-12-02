46bf4c96a53273fbef15691fedf12d834ad0e10b
SJCMN <sam.clavette@gmail.com>
2021-12-02 14:37:35 -0600

    Added basic GET and POST API's

--- Controllers/PetOwnersController.cs
+++ Controllers/PetOwnersController.cs
@@ -19,8 +19,17 @@ namespace pet_hotel.Controllers
         // This is just a stub for GET / to prevent any weird frontend errors that 
         // occur when the route is missing in this controller
         [HttpGet]
-        public IEnumerable<PetOwner> GetPets() {
-            return new List<PetOwner>();
+        public IEnumerable<PetOwner> GetAll() {
+            return _context.PetOwners;
+        }
+
+        [HttpPost]
+
+        public IActionResult Post(PetOwner petOwner)
+        {
+            _context.Add(petOwner);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(Post), new {id = petOwner.id}, petOwner);
         }
     }
 }
