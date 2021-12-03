88c7d4830e87a24ed7e55f1a61d4ab6de4329c84
Hamsa Alimci <alimici2016@gmail.com>
2021-12-03 09:51:48 -0600

    Added some console.writeline, to check for a bug that never existed in the first place

--- Controllers/PetsController.cs
+++ Controllers/PetsController.cs
@@ -15,14 +15,16 @@ namespace pet_hotel.Controllers
     public class PetsController : ControllerBase
     {
         private readonly ApplicationContext _context;
-        public PetsController(ApplicationContext context) {
+        public PetsController(ApplicationContext context)
+        {
             _context = context;
         }
 
         // This is just a stub for GET / to prevent any weird frontend errors that 
         // occur when the route is missing in this controller
         [HttpGet]
-        public IEnumerable<Pet> GetPets() {
+        public IEnumerable<Pet> GetPets()
+        {
             return _context.Pets.Include(pet => pet.petOwner);
         }
 
@@ -32,10 +34,10 @@ namespace pet_hotel.Controllers
             _context.Add(pet);
             _context.SaveChanges();
 
-            return CreatedAtAction(nameof(Create), new {id = pet.id}, pet);
+            return CreatedAtAction(nameof(Create), new { id = pet.id }, pet);
         }
         [HttpGet("{id}")]
-        public Pet GetById (int id)
+        public Pet GetById(int id)
         {
             return _context.Pets
                 .Include(Pet => Pet.petOwner)
@@ -46,7 +48,8 @@ namespace pet_hotel.Controllers
         {
             Pet pet = _context.Pets.SingleOrDefault(pet => pet.id == id);
 
-            if (pet is null){
+            if (pet is null)
+            {
                 return NotFound();
             }
             _context.Pets.Remove(pet);
@@ -58,36 +61,17 @@ namespace pet_hotel.Controllers
 
         public IActionResult Update(int id, Pet pet)
         {
+            Console.WriteLine("Update", id);
+            Console.WriteLine("Update", pet.breed);
             if (id != pet.id)
+            {
+                Console.WriteLine(id.ToString(), pet.id);
                 return BadRequest();
+            }
+            _context.Update(pet);
+            _context.SaveChanges();
 
-                _context.Update(pet);
-                _context.SaveChanges();
-
-                return NoContent();
+            return NoContent();
         }
-        // [HttpGet]
-        // [Route("test")]
-        // public IEnumerable<Pet> GetPets() {
-        //     PetOwner blaine = new PetOwner{
-        //         name = "Blaine"
-        //     };
-
-        //     Pet newPet1 = new Pet {
-        //         name = "Big Dog",
-        //         petOwner = blaine,
-        //         color = PetColorType.Black,
-        //         breed = PetBreedType.Poodle,
-        //     };
-
-        //     Pet newPet2 = new Pet {
-        //         name = "Little Dog",
-        //         petOwner = blaine,
-        //         color = PetColorType.Golden,
-        //         breed = PetBreedType.Labrador,
-        //     };
-
-        //     return new List<Pet>{ newPet1, newPet2};
-        // }
     }
 }
